98b903a3f302ed9af763b3d1b0eb55314fa50f54
Tab-e-n <howdoesonename@gmail.com>
2023-05-17 01:24:15 +0200

    Bet on this being broken somehow

--- prompt_generator.py
+++ prompt_generator.py
@@ -3,54 +3,52 @@ import json
 import csv_device as db
 import AI_API
 
+# This function will load the database, trim it according to the functions specifications and generate a OpenAI prompt out of the data.
 def generate_prompt(device_type: str, start: int, end: int, device_id: int = -1):
+    # Database searching
     database = db.load_database()
     database = db.timeframe(database, start, end)
     database = db.lookup(database, 2, device_type)
     if device_id != -1:
         database = db.lookup(database, 1, device_id)
-    #db.print_database(database)
-    
+
+    # If there are no results in the database, return info about that.
     if len(database) == 0:
         return "No Smart Home Status Report for " + device_type + "."
-    
+
+    # Generate prompt from the found data
     prompt = "Smart Home Status Report\n\n" + device_type + " Usage Reports:\n\n"
     for report in database:
         time = convert_number_to_time(int(report[3]))
         prompt += " - " + time[0] + "-" + time[1] + "-" + time[2] + " " + time[3] + ":" + time[4] + ":" + time[5] + ", device " + report[1] +":"
 
-        j = report[4]
-        j = j.replace("'", '"')
-        j = json.loads(j)
+        metadata = report[4]
+        metadata = metadata.replace("'", '"')
+        metadata = json.loads(metadata)
 
-        for i in j.keys():
+        for i in metadata.keys():
             match i:
                 case "power_consumption":
-                    prompt += " " + str(j[i]) + "kWh used since last report."
+                    prompt += " " + str(metadata[i]) + "kWh used since last report."
                 case "brightness":
-                    prompt += " Device was at " + str(j[i]) + "% brightness."
+                    prompt += " Device was at " + str(metadata[i]) + "% brightness."
                 case "temperature":
-                    prompt += " Temperature was at " + str(j[i]) + "°C."
+                    prompt += " Temperature was at " + str(metadata[i]) + "°C."
                 case "humidity":
-                    prompt += " Humidity was at " + str(j[i]) + "%."
+                    prompt += " Humidity was at " + str(metadata[i]) + "%."
                 case "status":
-                    prompt += ' Devices status: "' + str(j[i]) + '".'
+                    prompt += ' Devices status: "' + str(metadata[i]) + '".'
                 case "volume":
-                    prompt += " The volume was at " + str(j[i]) + "%."
+                    prompt += " The volume was at " + str(metadata[i]) + "%."
                 # If need be you can easily add more special responses by adding more cases to this match statement
                 case other:
-                    prompt += ' "' + i + '" was "' + str(j[i]) + '".'
+                    prompt += ' "' + i + '" was "' + str(metadata[i]) + '".'
                     
-                
-        #else:
-        #    prompt += "Power consumption wasn't specified or isn't applicable."
-        
         prompt += "\n"
     prompt += "\nQuestion: How can i optimize the usage of my smart house system based on these patterns?"
     return prompt
 
-# 2023 05 23 11 38 02
-
+# This function converts numbers like this: 20230715153000 into an array with a info about the reports year, month, day, hour, minute and second.
 def convert_number_to_time(number: int):
     return [
         str(number // 10000000000),
@@ -60,15 +58,3 @@ def convert_number_to_time(number: int):
         str(number // 1000 % 10) + str(number // 100 % 10), 
         str(number // 10 % 10) + str(number % 10), 
     ]
-"""
-print(AI_API.get_help(generate_prompt("TV", 0, 20231230235959)))
-print("")
-
-print(AI_API.get_help(generate_prompt("LIGHT", 0, 20231230235959)))
-print("")
-print(AI_API.get_help(generate_prompt("DOOR_SENSOR", 0, 20231230235959)))
-print("")
-print(AI_API.get_help(generate_prompt("HEATING", 0, 20231230235959)))
-
-"""
-print(AI_API.get_help(""))
