645c3fbf903f91e7b389cd07a28fb847a01ec097
Tab-e-n <howdoesonename@gmail.com>
2023-05-16 13:55:59 +0200

    csv_device fix

--- csv_device.py
+++ csv_device.py
@@ -1,21 +1,27 @@
+import os
 import json
 import csv
 
 def save_json_to_csv(data, filename):
-    f = csv.writer(open(filename, "w+"))
-    h = []
+
+    if os.stat(filename).st_size == 0:
+        f = csv.writer(open(filename, "w+"))
+        h = list(data)
+        print(len(h))
+        for i in range(len(h)):
+            print(i)
+            if h[i] == "COMMENTS":
+                h.pop(i)
+                break
+        f.writerow(h)
+    
+    f = csv.writer(open(filename, "a+"))
     a = []
     for i in list(data):
         if i != "COMMENTS":
             #print(i, " : ", data[i])
-            h.append(i)
             a.append(data[i])
     #print(a)
-    f.writerow(h)
     f.writerow(a)
-
-def add_header(filename):
-    if os.stat(filename).st_size == 0:
-            writer = csv.writer(file)
-            writer.writerow(['CONNECTION_ID', 'DEVICE_ID', 'DEVICE_TYPE', 'TIME_STAMP', 'METADATA')
     
+
