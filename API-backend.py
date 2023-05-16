e7587ba73b21a141aa1f71688889c6b554be1833
Tersus <terssuss@gmail.com>
2023-05-16 17:05:53 +0200

    Added function to locate devices using device_id

--- API-backend.py
+++ API-backend.py
@@ -1,9 +1,24 @@
 from flask import Flask, jsonify
+<<<<<<< Updated upstream
 app = Flask(__name__)
 
 devices = [{'TV':'true', 'lamp':'false', 'oven':'false'}]
 data = {}
+=======
+import csv
+#import csv_device as db
 
+app = Flask(__name__)
+
+devices = {'TV':'off', 'radio':'on', 'lamp':'off'}
+>>>>>>> Stashed changes
+
+def find_device_in_database(id):
+    with open('devices.csv', 'r') as file:
+        for line in file:
+            if line.find(id) != -1:
+                return 
+                
 
 @app.route('/api/devices', methods=['GET'])
 def get_devices():
--- csv_device.py
+++ csv_device.py
@@ -2,7 +2,13 @@ import os
 import json
 import csv
 
+<<<<<<< Updated upstream
 def save_json_to_csv(data, filename):
+=======
+load_dotenv("locales.env")
+device_filename = str(os.getenv('deviceDatabase', default="devices.csv"))
+signals_filename = str(os.getenv('deviceSignalsDatabase', default="databaze.csv"))
+>>>>>>> Stashed changes
 
     if os.stat(filename).st_size == 0:
         f = csv.writer(open(filename, "w+"))
