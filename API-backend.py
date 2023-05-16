9b12843d904d84b8e977f80e30010a673f47cd7c
Tersus <terssuss@gmail.com>
2023-05-17 00:47:22 +0200

    Improved commenting

--- API-backend.py
+++ API-backend.py
@@ -13,7 +13,7 @@ device_filename = str(os.getenv('deviceDatabase', default="devices.csv"))
 
 app = Flask(__name__)
 
-# funkce pro hledani zarizeni podle device_id
+# fuction for locating specific devices in the database using device_id
 def find_device_in_database(id:int):
     with open(device_filename, 'r') as file:
         csv_file = csv.reader(file)
@@ -21,23 +21,23 @@ def find_device_in_database(id:int):
             if line[0] == id:
                 return line[1]
             
-# funkce pro hledani zarizeni podle device_id
+# function for locating device metadata in the database using device_id
 def find_metadata_in_database(id:int):
     with open('databaze.csv', 'r') as file:
         main_list = []
         metadata = {}
         csv_file = csv.reader(file)
-        
+        # "turns" the file into a list
         for i in csv_file:
             main_list.append(i)
-
+        # locates the requested device_id and returns the newest metadata
         for line in range(len(main_list) - 1):
             if (int(main_list[-1-line][1]) == int(id)):
                 metadata = main_list[-1-line][4]
                 return metadata
         return "error"
 
-#funcke pro zjisteni pripojenych zarizeni
+# locates connected devices in the database 
 def connected_devices():
     devices = []
     with open(device_filename, 'r') as file:
@@ -48,22 +48,17 @@ def connected_devices():
             devices.append(line[1])
         return devices
 
-# vrati nazvy vsech momentalne pripojenych zarizeni
+# prints out the name of all the connected devices
 @app.route('/api/devices', methods=['GET'])
 def get_devices():
-    # tvorba odpovedi v JSON formatu
+    # turns all the connected devices into JSON format and returns
     devices = connected_devices()
     response = {"devices":devices}
-    
-    # vraci hodnoty v JSON formatu
     return jsonify(response)
 
-
-# zobrazi detaily specifickeho zarizeni podle zadane device_id
+# prints out the name and metadata of a specified device using device_id
 @app.route('/api/device/<id>', methods=['GET'])
 def get_device(id):
-
-    # prirazeni zarizeni podle id do promenne device
     device = find_device_in_database(id)
     device_metadata = find_metadata_in_database(id)
 
@@ -73,24 +68,25 @@ def get_device(id):
         return jsonify(response)
 
     else:
-        # the return of the JSON response
         response = {'device':device, 'metadata':device_metadata}
         return jsonify(response)
 
-# vrati ai generovany napad na zlepseni elektricke otazky
+# returns ai generated answer on how to improve electricity consumption 
 @app.route('/api/suggestion', methods=['GET'])
 def get_ai_suggestion(device_type, start, end, device_id = -1):
     prompt = prompt_generator.generate_prompt(device_type, start, end, device_id)
     return AI_API.get_help(prompt)
 
+# registers a new device with a new device_id
 @app.route('/api/device/<id>', methods=['POST'])
 def post_device(connect_id, id, type):
     database_management.add_device(id, connect_id, type)
 
-# function for deleting specific devices using the device_id
+# deletes specific devices using the device_id
 @app.route('/api/device/<id>', methods=['DELETE'])
 def delete_device(connect_id, id, type):
     database_management.remove_device(id, connect_id, type)
+
 
 @app.route('/api/wled/<id>', methods=['POST'])
 def change_led_state(device_id: int, connection_id: int, led_state: bool, led_rgb: list):
